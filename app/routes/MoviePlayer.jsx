6e1075ccca410c9b9d999c3404980f246280964c
Kirito8400 <kirito.dev0084@gmail.com>
2025-05-14 00:26:00 +0530

    mxdrop test

--- app/routes/MoviePlayer.jsx
+++ app/routes/MoviePlayer.jsx
@@ -1,16 +1,35 @@
 import { useSearchParams } from "@remix-run/react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export default function VideoPlayer() {
+  const [videoData, setVideoData] = useState(null);
+  const [videoId, setVideoId] = useState("");
   const [searchParams] = useSearchParams();
-  const movieId = searchParams.get("id");
-  console.log("movieId", movieId);
-  const videoId = movieId || "dQw4w9WgXcQ"; // Example video ID
+  const movieId = searchParams.get("id") || "x9jf786";
+
+  useEffect(() => {
+    const fetchVideoData = async () => {
+      try {
+        const url = `https://api.dailymotion.com/video/${movieId}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        setVideoData(data);
+        setVideoId(data.id);
+      } catch (error) {
+        console.error("Error fetching video data:", error);
+      }
+    };
+
+    console.log("videoId", videoId);
+
+    fetchVideoData();
+  }, [movieId]);
+
   const onClose = () => {
-    // Logic to close the video player
     console.log("Video player closed");
     window.history.back();
   };
+
   return (
     <div className="fixed h-80vh inset-0 z-50 bg-black/90 flex items-center justify-center">
       {/* Close button */}
@@ -38,12 +57,6 @@ export default function VideoPlayer() {
       {/* Video iframe */}
       <div className="w-full h-full md:w-[90%] md:h-[80vh] relative">
         {/* <iframe
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-            className="absolute inset-0 w-full h-full"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-        ></iframe> */}
-        <iframe
           src="https://player.vimeo.com/video/1083678311?h=81d1858b32&amp;title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
           width="1280"
           height="528"
@@ -51,6 +64,27 @@ export default function VideoPlayer() {
           frameborder="0"
           allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
           title="4_5771482232371810792"
+        ></iframe> */}
+
+        {/* <iframe
+          // src={`https://geo.dailymotion.com/player.html?video=${movieId}&autoplay=1`}
+          // src="https://geo.dailymotion.com/player.html?video=x9jf786"
+          className="absolute inset-0 w-full h-full"
+          width="1280"
+          height="528"
+          title="Dailymotion Video Player"
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write; encrypted-media"
+          allowFullScreen
+        ></iframe> */}
+        <iframe
+          // width="640"
+          className="absolute inset-0 w-full h-full"
+          // height="480"
+          src="//mxdrop.to/e/el14x1pmb8gvll"
+          // scrolling="no"
+          // frameborder="0"
+          allowfullscreen
         ></iframe>
       </div>
     </div>
